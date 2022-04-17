597eca9b837d8590e08abb8a3b15a244f685798e
Marcelo Cohen <mflashbr@gmail.com>
2022-04-17 19:56:45 -0300

    First commit: exemplo de temporização

---
+++ Main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var total : float = 0
+
+func _ready() -> void:
+	update_score(total)
+
+func _process(delta: float) -> void:
+	total += delta
+	update_score(total)
+	
+func update_score(current_score: float) -> void:
+	$Score.text = str(current_score)
+	
+	
